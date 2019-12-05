2be269f46ee24d34af25f7cc31b72dcd42bb70da
Josh Francisco <fjoshcarl@yahoo.com>
2019-12-05 11:46:20 +0800

    Update WebsocketDemoEvent.php

--- app/Events/WebsocketDemoEvent.php
+++ app/Events/WebsocketDemoEvent.php
@@ -1,19 +1,15 @@
 <?php
-
 namespace App\Events;
-
 use Illuminate\Broadcasting\Channel;
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
-use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
-
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 class WebsocketDemoEvent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
-
     public $somedata;
     /**
      * Create a new event instance.
@@ -24,7 +20,6 @@ class WebsocketDemoEvent implements ShouldBroadcast
     {
         $this->somedata = $somedata;
     }
-
     /**
      * Get the channels the event should broadcast on.
      *
